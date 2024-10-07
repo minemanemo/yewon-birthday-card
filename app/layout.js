697e1ff034de4520ee306633cb323e8025e6c3ca
MINHOE LEE <minhoe1122@gmail.com>
2024-10-07 19:15:03 +0900

    Init

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const geAmsterdam = localFont({
+  src: "./fonts/font.ttf",
+  variable: "--font-amsterdam",
+  weight: "100 700 900",
 });
 
 export const metadata = {
@@ -19,8 +14,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="ko">
+      <body className={`${geAmsterdam.variable}`}>
         {children}
       </body>
     </html>
